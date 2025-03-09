e369d2242386158e8e5319d20f8466cf9c0dc289
MarLen <y.duan@imperial.ac.uk>
2025-03-09 23:14:19 +0000

    updated normaura.sh

--- normaura.sh
+++ normaura.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Navigate to the directory containing your Streamlit app (if needed)
-# cd /path/to/your/app
+# Get the directory where the .sh file is located
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-# Run the Streamlit app
-streamlit run ./NormAura.py
+# Construct the full path to NormAura.py
+NORM_AURA_PATH="$SCRIPT_DIR/NormAura.py"
+
+# Run the Streamlit app using the full path
+streamlit run "$NORM_AURA_PATH"
+
